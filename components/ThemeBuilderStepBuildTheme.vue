634658de0c81545975dd0bccdc0f037d595677dd
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-05-23 13:55:02 -0500

    Add header to the theme step

--- components/ThemeBuilderStepBuildTheme.vue
+++ components/ThemeBuilderStepBuildTheme.vue
@@ -1,15 +1,21 @@
 <template>
-  <div class="border rounded">
-    <theme-builder-step-build-theme-component
-      v-for="(componentTheme, componentName) in currentTheme"
-      :key="componentName"
-      v-model="currentTheme[componentName]"
-      :index="Object.keys(currentTheme).indexOf(componentName)"
-      :component-name="componentName"
-      :selected="componentName === selected"
-      @select="selected = componentName"
-      @next="selectNextComponent"
-    />
+  <div>
+    <h2 class="text-2xl font-medium text-gray-900">
+      Personalize your component CSS classes
+    </h2>
+    <p>Browse among the components you chose and assign them a list of default classes as well as all the variants you need.</p>
+    <div class="border rounded mt-3">
+      <theme-builder-step-build-theme-component
+        v-for="(componentTheme, componentName) in currentTheme"
+        :key="componentName"
+        v-model="currentTheme[componentName]"
+        :index="Object.keys(currentTheme).indexOf(componentName)"
+        :component-name="componentName"
+        :selected="componentName === selected"
+        @select="selected = componentName"
+        @next="selectNextComponent"
+      />
+    </div>
   </div>
 </template>
 <script>
