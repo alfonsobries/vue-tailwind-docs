97c561c708258b1886c69ebadeefa3f09e74182a
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-05-23 17:04:56 -0500

    Copy code

---
+++ components/Alert.vue
@@ -0,0 +1,63 @@
+<template>
+  <div
+    class="rounded-md  p-4 mb-4"
+    :class="{
+      'bg-green-100': type === 'success',
+      'bg-red-100': type === 'error',
+    }"
+  >
+    <div class="flex">
+      <div class="flex-shrink-0">
+        <icon v-if="type === 'success'" class="h-5 w-5 text-green-400">
+          <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+        </icon>
+        <icon v-if="type === 'error'" class="h-5 w-5 text-red-400">
+          <path id="Combined-Shape" d="M2.92893219,17.0710678 C6.83417511,20.9763107 13.1658249,20.9763107 17.0710678,17.0710678 C20.9763107,13.1658249 20.9763107,6.83417511 17.0710678,2.92893219 C13.1658249,-0.976310729 6.83417511,-0.976310729 2.92893219,2.92893219 C-0.976310729,6.83417511 -0.976310729,13.1658249 2.92893219,17.0710678 Z M9,5 L11,5 L11,11 L9,11 L9,5 Z M9,13 L11,13 L11,15 L9,15 L9,13 Z" />
+        </icon>
+      </div>
+      <div class="ml-3">
+        <p
+          class="text-sm leading-5 font-medium "
+          :class="{
+            'text-green-800': type === 'success',
+            'text-red-800': type === 'error',
+          }"
+        >
+          <slot />
+        </p>
+      </div>
+      <div class="ml-auto pl-3">
+        <div class="-mx-1.5 -my-1.5">
+          <button
+            class="inline-flex rounded-md p-1.5 focus:outline-none focus:bg-green-100 transition ease-in-out duration-150"
+            :class="{
+              'text-green-500 hover:bg-green-100': type === 'success',
+              'text-red-500 hover:bg-red-100': type === 'error',
+            }"
+            @click="$emit('close')"
+          >
+            <icon class="h-4 w-4">
+              <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+            </icon>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Icon from '@/components/Icon'
+
+export default {
+  components: {
+    Icon
+  },
+  props: {
+    type: {
+      type: String,
+      default: 'success'
+    }
+  }
+}
+</script>
--- components/ThemeBuilderStepInstall.vue
+++ components/ThemeBuilderStepInstall.vue
@@ -5,9 +5,17 @@
     </h2>
     <p>Select the components that you want to personalize</p>
     <hr class="border-t my-4">
+
+    <alert v-if="copied === true" type="success" @close="copied = undefined">
+      The code was copied correctly!
+    </alert>
+    <alert v-if="copied === false" type="error" @close="copied = undefined">
+      Something went wrong! We could not copy the code try to copy it manually.
+    </alert>
+
     <div class="relative">
       <div class="absolute right-0 top-0 m-4 flex items-center">
-        <button type="button" class="text-gray-500 hover:text-orange-500 flex items-center text-sm">
+        <button type="button" class="text-gray-500 hover:text-orange-500 flex items-center text-sm" @click="copyCode">
           <icon class="h-5 w-5 mr-2 ">
             <path id="Combined-Shape" d="M0,8.00585866 C0,6.89805351 0.897060126,6 2.00585866,6 L11.9941413,6 C13.1019465,6 14,6.89706013 14,8.00585866 L14,17.9941413 C14,19.1019465 13.1029399,20 11.9941413,20 L2.00585866,20 C0.898053512,20 0,19.1029399 0,17.9941413 L0,8.00585866 L0,8.00585866 Z M6,8 L2,8 L2,18 L12,18 L12,14 L15,14 L15,12 L18,12 L18,2 L8,2 L8,5 L6,5 L6,8 L12,8 L12,14 L17.9941413,14 C19.1029399,14 20,13.1019465 20,11.9941413 L20,2.00585866 C20,0.897060126 19.1019465,0 17.9941413,0 L8.00585866,0 C6.89706013,0 6,0.898053512 6,2.00585866 L6,8 Z" />
           </icon>
@@ -21,6 +29,7 @@
           Download
         </button>
       </div>
+
       <vue-code-highlight>{{ code }}</vue-code-highlight>
     </div>
   </div>
@@ -28,17 +37,25 @@
 <script>
 import Vue from 'vue'
 import { component as VueCodeHighlight } from 'vue-code-highlight'
+import copy from 'clipboard-copy'
+import Alert from '@/components/Alert'
 import Icon from '@/components/Icon'
 
 export default Vue.extend({
   components: {
     VueCodeHighlight,
+    Alert,
     Icon
   },
   props: {
     theme: {
       type: Object,
       required: true
+    }
+  },
+  data () {
+    return {
+      copied: undefined
     }
   },
   computed: {
@@ -57,6 +74,14 @@ Vue.use(VueTailwind, theme)
     }
   },
   methods: {
+    copyCode () {
+      copy(this.code)
+        .then(() => {
+          this.copied = true
+        }).catch(() => {
+          this.copied = false
+        })
+    },
     downloadCode () {
       const element = document.createElement('a')
       element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(this.code))
