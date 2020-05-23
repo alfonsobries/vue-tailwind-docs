d6043e2108ba2898f6b0d406f8b236fc33f17c1f
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-05-23 16:41:24 -0500

    Custom theme for code highlight

--- components/ThemeBuilderStepInstall.vue
+++ components/ThemeBuilderStepInstall.vue
@@ -5,20 +5,36 @@
     </h2>
     <p>Select the components that you want to personalize</p>
     <hr class="border-t my-4">
-    <fieldset>
-      <pre>
-        {{ theme }}
-      </pre>
-    </fieldset>
+    <vue-code-highlight>{{ code }}</vue-code-highlight>
   </div>
 </template>
 <script>
 import Vue from 'vue'
+import { component as VueCodeHighlight } from 'vue-code-highlight'
+
 export default Vue.extend({
+  components: {
+    VueCodeHighlight
+  },
   props: {
     theme: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    code () {
+      const theme = JSON.stringify(this.theme, null, 2)
+        .replace(/"([^"]+)":/g, '$1:')
+        .replace(/\"/g, '\'')
+      return `import Vue from 'vue'
+import VueTailwind from 'vue-tailwind'
+
+const theme = ${theme}
+
+Vue.use(VueTailwind, theme)
+
+`
     }
   }
 })
