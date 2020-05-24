5c63a836da0e99222906334a1b4f8483684a704e
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-05-24 15:08:05 -0500

    Disable purgecss

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
+  purge: {
+    enabled: false
+  },
   theme: {},
   variants: {
     textColor: ['responsive', 'hover', 'focus', 'group-hover']
