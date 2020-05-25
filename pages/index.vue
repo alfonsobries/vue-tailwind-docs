e9afd2b2f3ca184a7605b9b0ac4f8315fc077638
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-05-25 16:45:49 -0500

    Presentation

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,65 @@
 <template>
   <div>
-    <div class="bg-gray-300 py-6 shadow-lg">
-      <div class="container mx-auto">
+    <div class="bg-orange-100 py-16 shadow-lg">
+      <div class="container mx-auto max-w-2xl">
         <h3 class="text-3xl font-semibold">
-          Hi Adam!,
+          👋 Hi Adam!,
         </h3>
 
-        <p>
-          My name is Alfonso Bribiesca, I am a full stack software developer from Mexico City and that is hoping to join to the Tailwind team, in order to be considered I do my best to make time to made this small project:
+        <p class="mt-4">
+          My name is <a href="https://alfonsobribiesca.com/" target="_blank" class="border-b-4 border-orange-300 hover:text-orange-600 text-orange-500">👨‍💻 Alfonso Bribiesca</a> I am a full-stack software developer from Mexico City with about 12 years of experience, and I want to join to the TailwindCSS team:
         </p>
 
-        <ol class="p-4 list-disc">
-          <li class="my-2">
-            First: I made from scratch a new version of an old open source package called VueTailwind based in my experience working every single project with Tailwind and
+        <p class="mt-4">
+          To be considered and as the job description asks for, I developed this project that I hope can serve as a useful reference for my work. I build this project in 2 parts:
+        </p>
+
+        <ol class="px-4 list-decimal">
+          <li class="mt-4">
+            I re-think and re-build from scratch in Typescript, a new, improved version of an old package that I create a year ago called VueTailwind that is a set of common components that are meant to be configured to have personalized CSS classes.
+            You can see the <a href="https://v1.vue-tailwind.com/" target="_blank" class="border-b-4 border-orange-300 hover:text-orange-600 text-orange-500">new docs here</a> and the <a href="https://github.com/alfonsobries/vue-tailwind" target="_blank" class="border-b-4 border-orange-300 hover:text-orange-600 text-orange-500">source code here</a>
           </li>
-          <li class="my-2">
-            Second: I made this website where the user can interactively <a href="#build" class="underline">create his own themes</a> of the components the library has.
+          <li class="mt-4">
+            For that new package, I create a new Theme builder that allows the users to create and preview his theme using an autocomplete input that gets TailwindCSS classes fetched from a Laravel API. You can <a href="#theme-builder" class="border-b-4 border-dotted border-orange-300 hover:text-orange-600 text-orange-500" @click.prevent="smoothScrolToThemeBuilder">test it below ↓↓</a>.
           </li>
         </ol>
 
-        <p>My vision is</p>
-
-        <p>
-          I have all the skills you mention an a good amount of projects made with Tailwind and different techs, if you want I prepare this <a class="underline">Small resume</a>
-
-        <!-- <p>This is me, (im more handsome in person according - according to my mom)</p>
-
-        <img class="rounded-lg" src="https://www.alfonsobribiesca.com/photo.png"> -->
+        <p class="mt-4 font-semibold">
+          What I expect from this job and my vision for TailwindCSS
         </p>
+
+        <p class="mt-4">
+          I will like to create tools that help developers to create beautiful, fast, and customized applications with TailwindCSS-related libraries. I can easily picture TailwindCSS as the most import CSS framework in the near future, and I want to be part of it.
+        </p>
+
+        <p class="mt-4 font-semibold">
+          A little more about me:
+        </p>
+
+        <ol class="px-4 list-disc">
+          <li class="mt-4">
+            I am an expert in VueJS and Laravel together with his ecosystem(is my default stack). However, I know different technologies needed for this team like React and Next.js; my experience in both is not as extensive as VueJs. Still, I'm ready to create any project with both techs or different ones. (You can learn more about my knowledge in <a class="border-b-4 border-orange-300 hover:text-orange-600 text-orange-500" target="_blank" href="https://alfonsobribiesca.com/">my resume</a>)
+          </li>
+          <li class="mt-4">
+            I have been independently making web applications for 12 years on behalf of my company called <a href="https://www.vexilo.com/en" target="_blank" class="border-b-4 border-orange-300 hover:text-orange-600 text-orange-500">Vexilo</a>. (Quick note: even though it sells as a "Company" is a one-person company (me) that means all the portfolio is mine).
+          </li>
+          <li class="mt-4">
+            I am self-motivated, used to work independently, and a person who is making an effort to learn new stuff regularly.
+          </li>
+          <li class="mt-4">
+            For the last two years, "my day" job has been for a US company called Surg.io. I'm used to working together with an English-speaking team (As you may imagine, my native language is Spanish).
+          </li>
+          <li class="mt-4">
+            I am a big fan of TailwindCSS, and his philosophy, and as a small proof, every single link that you can find in this page is for something built with TailwindCSS. Also, I have been a follower of your work for a couple of years now; I buy and take both of your courses (Test-Driven Laravel & Refactoring to Collections) and, of course, read the Refactoring UI book that you write together with Steve Schoger.
+          </li>
+          <li class="mt-4">
+            PD: I love Canada 🇨🇦
+          </li>
+        </ol>
       </div>
     </div>
 
-    <div class="bg-gray-100 py-6">
+    <div id="theme-builder-section" class="bg-gray-100 py-16">
       <header>
         <div class="container mx-auto md:px-0">
           <h2 id="build" class="text-3xl text-center font-semibold text-gray-900 mb-4">
@@ -53,6 +81,11 @@ import ThemeBuilder from '~/components/ThemeBuilder.vue'
 export default Vue.extend({
   components: {
     ThemeBuilder
+  },
+  methods: {
+    smoothScrolToThemeBuilder () {
+      document.getElementById('theme-builder-section')!.scrollIntoView({ behavior: 'smooth' })
+    }
   }
 })
 </script>
