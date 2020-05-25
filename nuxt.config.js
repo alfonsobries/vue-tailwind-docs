6cbd1a804136c116929684c5a4bdf95b4a6f79ef
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-05-25 14:22:37 -0500

    And title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: 'VueTailwind',
+    title: 'VueTailwind - Customizable Vue Components for TailwindCSS',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
