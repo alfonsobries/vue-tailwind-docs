6b1d7bbaa04e954aaa392185d1ff6f799dd5cc31
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-07-29 15:41:26 -0500

    minor ui fixes

--- pages/contribute.vue
+++ pages/contribute.vue
@@ -1,43 +1,45 @@
 <template>
-  <div class="mx-auto max-w-screen-xl px-4 sm:px-6 pb-10 py-10">
-    <div class="max-w-xl mx-auto">
-      <h1 class="mb-10 leading-none text-gray-900 font-medium text-3xl tracking-tight">
-        Contribute to this project
-      </h1>
-      <h3 class="text-xl font-semibold">
-        <span class="text-5xl">👨‍💻</span> Hey! Alfonso here
-      </h3>
+  <div class="bg-gray-100">
+    <div class="mx-auto max-w-screen-xl px-4 sm:px-6 pb-10 py-10">
+      <div class="max-w-xl mx-auto">
+        <h1 class="mb-10 leading-none text-gray-900 font-medium text-3xl tracking-tight text-center">
+          Contribute to this project
+        </h1>
+        <h3 class="text-xl font-semibold">
+          <span class="text-5xl">👨‍💻</span> Hey! Alfonso here
+        </h3>
 
-      <p class="mt-4">
-        I spend hundreds of hours in this project to challenge myself to make the greatest and more flexible library of UI components for CSS utility-first frameworks like TailwindCSS.
-      </p>
+        <p class="mt-4">
+          I spend hundreds of hours in this project to challenge myself to make the greatest and more flexible library of UI components for CSS utility-first frameworks like TailwindCSS.
+        </p>
 
-      <p class="mt-4">
-        As you may imagine, this project represents a lot of work and different associated costs.
-      </p>
+        <p class="mt-4">
+          As you may imagine, this project represents a lot of work and different associated costs.
+        </p>
 
-      <p class="mt-4">
-        Is this project is helpful for you can help in different ways:
-      </p>
+        <p class="mt-4">
+          Is this project is helpful for you can help in different ways:
+        </p>
 
-      <ol class="px-4 list-decimal">
-        <li class="mt-4 flex">
-          <span class="block -ml-5 mr-4 text-3xl">⭐️</span>
-          <span>The easiest way to help is by giving me a start in Github <a class="underline border-orange-300 hover:text-orange-600 text-orange-500" href="https://github.com/alfonsobries/vue-tailwind" target="_blank">https://github.com/alfonsobries/vue-tailwind</a></span>
-        </li>
-        <li class="mt-4 flex">
-          <span class="block -ml-5 mr-4 text-3xl">🔧</span>
-          <span>You can help to add new features or fixing bugs in Github <a class="underline border-orange-300 hover:text-orange-600 text-orange-500" href="https://github.com/alfonsobries/vue-tailwind" target="_blank">https://github.com/alfonsobries/vue-tailwind</a></span>
-        </li>
-        <li class="mt-4 flex">
-          <span class="block -ml-5 mr-4 text-3xl">📝</span>
-          <span>Improve the documentation by fixing missing or wrong information, typos, or grammar errors (I'm not a native English speaker). Check the source code here: <a class="underline border-orange-300 hover:text-orange-600 text-orange-500" target="_blank" href="https://github.com/alfonsobries/vue-tailwind-themes">https://github.com/alfonsobries/vue-tailwind-themes</a></span>
-        </li>
-        <li class="mt-4 flex">
-          <span class="block -ml-5 mr-4 text-3xl">💵</span>
-          <span>One of the best ways to help me is by sponsoring me in GitHub <a class="underline border-orange-300 hover:text-orange-600 text-orange-500" target="_blank" href="https://github.com/sponsors/alfonsobries">https://github.com/sponsors/alfonsobries</a> so I can spend more time in this project and still pay the bills.</span>
-        </li>
-      </ol>
+        <ol class="px-4 list-decimal">
+          <li class="mt-4 flex">
+            <span class="block -ml-5 mr-4 text-3xl">⭐️</span>
+            <span>The easiest way to help is by giving me a start in Github <a class="underline border-orange-300 hover:text-orange-600 text-orange-500" href="https://github.com/alfonsobries/vue-tailwind" target="_blank">https://github.com/alfonsobries/vue-tailwind</a></span>
+          </li>
+          <li class="mt-4 flex">
+            <span class="block -ml-5 mr-4 text-3xl">🔧</span>
+            <span>You can help to add new features or fixing bugs in Github <a class="underline border-orange-300 hover:text-orange-600 text-orange-500" href="https://github.com/alfonsobries/vue-tailwind" target="_blank">https://github.com/alfonsobries/vue-tailwind</a></span>
+          </li>
+          <li class="mt-4 flex">
+            <span class="block -ml-5 mr-4 text-3xl">📝</span>
+            <span>Improve the documentation by fixing missing or wrong information, typos, or grammar errors (I'm not a native English speaker). Check the source code here: <a class="underline border-orange-300 hover:text-orange-600 text-orange-500" target="_blank" href="https://github.com/alfonsobries/vue-tailwind-themes">https://github.com/alfonsobries/vue-tailwind-themes</a></span>
+          </li>
+          <li class="mt-4 flex">
+            <span class="block -ml-5 mr-4 text-3xl">💵</span>
+            <span>One of the best ways to help me is by sponsoring me in GitHub <a class="underline border-orange-300 hover:text-orange-600 text-orange-500" target="_blank" href="https://github.com/sponsors/alfonsobries">https://github.com/sponsors/alfonsobries</a> so I can spend more time in this project and still pay the bills.</span>
+          </li>
+        </ol>
+      </div>
     </div>
   </div>
 </template>
--- pages/theme-builder.vue
+++ pages/theme-builder.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="mx-auto max-w-screen-xl px-4 sm:px-6 pb-10 bg-gray-100 py-10">
-    <h1 class="mb-10 leading-none text-gray-900 font-medium text-3xl tracking-tight">
-      Build your own theme
-    </h1>
+  <div class="bg-gray-100">
+    <div class="mx-auto max-w-screen-xl px-4 sm:px-6 pb-10 py-10">
+      <h1 class="mb-10 leading-none text-gray-900 font-medium text-3xl tracking-tight">
+        Build your own theme
+      </h1>
 
-    <theme-builder />
+      <theme-builder />
+    </div>
   </div>
 </template>
 
