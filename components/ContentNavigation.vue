67e57f480517431bf89ebf9454973d86642cf816
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-08-04 15:38:00 -0500

    Ensure it not closes the google ads tag

--- components/ContentNavigation.vue
+++ components/ContentNavigation.vue
@@ -24,7 +24,7 @@
           data-ad-slot="3494479226"
           data-ad-format="auto"
           data-full-width-responsive="true"
-        />
+        ><!-- --></ins>
         <script>
           (adsbygoogle = window.adsbygoogle || []).push({});
         </script>
