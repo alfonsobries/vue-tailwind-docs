d60b295b398952f2743ce9f29c3be557f9439cde
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-08-04 12:28:01 -0500

    Add google adds in sidebar

--- components/ContentNavigation.vue
+++ components/ContentNavigation.vue
@@ -16,6 +16,21 @@
       </ul>
 
       <div id="add-1" class="mt-10">
+        <!-- sidebar -->
+        <ins
+          class="adsbygoogle"
+          style="display:block"
+          data-ad-client="ca-pub-4751207776405212"
+          data-ad-slot="3494479226"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        />
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+      </div>
+
+      <div id="add-2" class="mt-10">
         <a target="_blank" href="https://www.vexilo.com/en/?utm_source=vuetailwind&amp;utm_medium=sidebar-widget" class="mt-3 block">
           <img src="@/assets/img/vexilo-add.png" alt="Nearshore software development company">
         </a>
@@ -31,7 +46,7 @@
         </div>
       </div>
 
-      <div id="add-1" class="mt-10 text-center">
+      <div id="add-3" class="mt-10 text-center">
         <t-button to="/contribute" class="flex items-center">
           <icon class="w-4 h-4 text-orange-200 mr-2">
             <path id="Fill-97" d="M11.0010436,0 C9.89589787,0 9.00000024,0.886706352 9.0000002,1.99810135 L9,8 L1.9973917,8 C0.894262725,8 0,8.88772964 0,10 L0,12 L2.29663334,18.1243554 C2.68509206,19.1602453 3.90195042,20 5.00853025,20 L12.9914698,20 C14.1007504,20 15,19.1125667 15,18.000385 L15,10 L12,3 L12,0 L11.0010436,0 L11.0010436,0 Z M17,10 L20,10 L20,20 L17,20 L17,10 L17,10 Z" />
