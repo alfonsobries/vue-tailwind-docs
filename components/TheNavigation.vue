247abb4ff3d13c45c5847a92492e9907abb0e225
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-08-10 09:46:33 -0500

    Fix search box

--- components/TheNavigation.vue
+++ components/TheNavigation.vue
@@ -36,7 +36,7 @@
           </div>
           <div class="flex-1 flex justify-center px-2 lg:ml-6 lg:justify-end">
             <label for="search" class="sr-only">Search</label>
-            <div class="relative">
+            <div class="relative w-full">
               <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10">
                 <svg class="h-5 w-5 text-gray-700" fill="currentColor" viewBox="0 0 20 20">
                   <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
