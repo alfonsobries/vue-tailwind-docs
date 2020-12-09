8d4e9f49f1cfa3d204f2b6a0bb98d92cbc3cdd4f
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-12-08 19:52:38 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: {
-    enabled: false
-  },
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
