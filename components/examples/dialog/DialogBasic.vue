587c5b539f159fc75805b0dd594dbb8e4d13794e
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-12-09 17:23:31 -0600

    Fix dialog example

--- components/examples/dialog/DialogBasic.vue
+++ components/examples/dialog/DialogBasic.vue
@@ -11,16 +11,9 @@
       type="confirm"
       name="named-dialog"
       icon="question"
+      title="Delete user?"
     >
-      <template slot="title">
-        Delete user?
-      </template>
-
       <p>This action cannot be undone.</p>
-
-      <template slot="icon">
-        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7a4 4 0 11-8 0 4 4 0 018 0zM9 14a6 6 0 00-6 6v1h12v-1a6 6 0 00-6-6zM21 12h-6" /></svg>
-      </template>
     </t-dialog>
   </preview>
 </template>
