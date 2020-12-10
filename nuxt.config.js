d9b003d79e1b01efe366b9d9cec6d195050ce4a8
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-12-09 21:04:08 -0600

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,15 @@ export default {
     '@plugins/exception-handler.js'
   ],
   build: {
-    extractCSS: true
+    extractCSS: true,
+    postcss: {
+      preset: {
+        features: {
+          // Fixes: https://github.com/tailwindcss/tailwindcss/issues/1190#issuecomment-546621554
+          'focus-within-pseudo-class': false
+        }
+      }
+    }
   },
   tailwindcss: {
     config: {
